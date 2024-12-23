21aedec0b4bf60c96238f721a678c42724430027
Sparsh Sethi <sparshsethi@outlook.com>
2024-12-23 11:26:57 +0530

    changing api key

--- LLM_Resume_Parser_App/components/llm_model.py
+++ LLM_Resume_Parser_App/components/llm_model.py
@@ -1,5 +1,5 @@
 from langchain_groq import ChatGroq
 
-llm = ChatGroq(temperature=0, model_name="llama3-8b-8192",groq_api_key="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",
+llm = ChatGroq(temperature=0, model_name="llama3-8b-8192",groq_api_key="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",
  model_kwargs={"response_format": {"type": "json_object"}})
 
